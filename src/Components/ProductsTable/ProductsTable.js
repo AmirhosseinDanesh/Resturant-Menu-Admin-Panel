96a6c615487354476b4260ae1d8c81421b954d6f
AmirDanesh <daneshahd78@gmail.com>
2023-02-21 00:08:44 +0330

    Get Products from Back-end

--- src/Components/ProductsTable/ProductsTable.js
+++ src/Components/ProductsTable/ProductsTable.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import "./ProductsTable.css"
 import DeleteModal from "../../Components/DeleteModal/DeleteModal"
 import DetailModals from '../DetailModals/DetailModals'
@@ -7,24 +7,31 @@ import EditModal from '../../Components/EditModal/EditModal';
 export default function ProductsTable() {
     const [isShowDeleteModal, setIsShowDeleteModal] = useState(false)
     const [isShowDetailModal, setIsShowDetailModal] = useState(false)
-    const [isShowEditModal , setIsShowEditModal] = useState(false)
+    const [isShowEditModal, setIsShowEditModal] = useState(false)
+    const [allProducts, setAllProducts] = useState([])
+
+    useEffect(() => {
+        fetch("http://localhost:8000/api/products/")
+            .then(res => res.json())
+            .then(products => setAllProducts(products))
+    } , [])
 
     const deleteModalCancel = () => {
         setIsShowDeleteModal(false)
     }
-    
+
     const deleteModalSubmit = () => {
         setIsShowDeleteModal(false)
     }
 
-    const closeDetailModal = ()=>{
+    const closeDetailModal = () => {
         setIsShowDetailModal(false)
     }
-    
-    const closeEditModal = ()=>{
+
+    const closeEditModal = () => {
         setIsShowEditModal(false)
     }
-    const submitEditModal = ()=>{
+    const submitEditModal = () => {
         setIsShowEditModal(false)
 
     }
@@ -44,34 +51,39 @@ export default function ProductsTable() {
                             </tr>
                         </thead>
                         <tbody>
-                            <tr style={{ verticalAlign: "middle" }}>
-                                <td>
-                                    <img className='tb-pr-img' src="./Images/iphone13.jpg" alt="" />
-                                </td>
-                                <td>
-                                    آیفون 13
-                                </td>
-                                <td>
-                                    2100000
-                                </td>
-                                <td>
-                                    5
-                                </td>
-                                <td>
-                                    <div className='d-flex justify-content-center'>
-                                        <button className="btn text-white ms-2 btn-sm btn-success" onClick={()=> setIsShowDetailModal(true)}>جزئیات</button>
-                                        <button className="btn text-white ms-2 btn-sm btn-danger" onClick={() => setIsShowDeleteModal(true)}>حذف</button>
-                                        <button className="btn text-white ms-2 btn-sm btn-primary" onClick={()=> setIsShowEditModal(true)}>ویرایش</button>
-                                    </div>
-                                </td>
-                            </tr>
+                            {
+                                allProducts.map((pr) => (
+                                    <tr key={pr.id} style={{ verticalAlign: "middle" }}>
+                                        <td>
+                                            <img className='tb-pr-img' src={pr.img} alt="" />
+                                        </td>
+                                        <td>
+                                            {pr.title}
+                                        </td>
+                                        <td>
+                                            {pr.price}
+                                        </td>
+                                        <td>
+                                            {pr.count}
+                                        </td>
+                                        <td>
+                                            <div className='d-flex justify-content-center'>
+                                                <button className="btn text-white ms-2 btn-sm btn-success" onClick={() => setIsShowDetailModal(true)}>جزئیات</button>
+                                                <button className="btn text-white ms-2 btn-sm btn-danger" onClick={() => setIsShowDeleteModal(true)}>حذف</button>
+                                                <button className="btn text-white ms-2 btn-sm btn-primary" onClick={() => setIsShowEditModal(true)}>ویرایش</button>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                ))
+                            }
+
                         </tbody>
                     </table>
                 </div>
             </div>
 
             {
-                isShowDeleteModal && <DeleteModal cancel={deleteModalCancel} submit={deleteModalSubmit}/>
+                isShowDeleteModal && <DeleteModal cancel={deleteModalCancel} submit={deleteModalSubmit} />
             }
 
             {
@@ -79,7 +91,7 @@ export default function ProductsTable() {
             }
 
             {
-                isShowEditModal && <EditModal onHide={closeEditModal} submit={submitEditModal} />  
+                isShowEditModal && <EditModal onHide={closeEditModal} submit={submitEditModal} />
             }
         </>
 
